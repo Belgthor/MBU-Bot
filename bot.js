7d342e255d9e83a52bba5ed3dc6cae7c3ef18764
Belgthor <44882812+Belgthor@users.noreply.github.com>
2018-11-08 21:19:05 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,10 +17,10 @@ client.on('message', message => {
 				message.channel.send('You need the \'Server Admin\' role');
 				return;
 			}
-			if (isNaN(args[0])) {
-				message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'purge <amount>');
-				return;
-			}
+			//if (isNaN(args[0])) {
+				//message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'purge <amount>');
+				//return;
+			//}
 			const fetched = await message.channel.fetchMessages()
 			message.channel.bulkDelete(fetched)
 				.catch(error => message.channel.send('Error: ' + error))
