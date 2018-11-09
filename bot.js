82f6b3f6a1ec7d1e182e43fd430ab6858d288b0b
Belgthor <44882812+Belgthor@users.noreply.github.com>
2018-11-08 20:38:39 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 	console.log('I am ready!');
 });
 client.on('message', message => {
-	if (!message.content.startsWith(prefix)) return;
+	if (!message.content.startsWith(prefix)) { return; }
 	let msg = message.content.toUpperCase();
 	let sender = message.author;
 	let cont = message.content.slice(prefix.length).split(' ');
@@ -13,10 +13,11 @@ client.on('message', message => {
 	if (msg.startsWith(prefix + 'PURGE')) {
 		async function purge(){
 			message.delete();
-			if (!message.member.roles.find('name', 'Server Admin'))
+			if (!message.member.roles.find('name', 'Server Admin')) {
 				message.channel.send('You need the \'Server Admin\' role');
 				return;
-			if (isNaN(args[0])){
+			}
+			if (isNaN(args[0])) {
 				message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'purge <amount>');
 				return;
 			}
