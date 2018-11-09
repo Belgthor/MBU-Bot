003ec80e399e6ee7bfd1161ca74fd9b169640940
Belgthor <44882812+Belgthor@users.noreply.github.com>
2018-11-08 20:10:09 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,16 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
- 
-
+const prefix = '!'
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+	console.log('I am ready!');
 });
-
- 
 client.on('message', message => {
-
-    if (message.content === '!ping') {
-
-       message.reply('pong');
-
-       }
-  if (message.content === '!purge') {
-    message.reply('command does not exist');
-  }
-
+	if (!message.content.startsWith(prefix)) return;
+	if (message.content.startsWith(prefix + 'purge')) {
+		message.channel.send('command does not exist');
+	} else
+	if (message.content.startsWith(prefix + 'ping')) {
+		message.channel.send('pong');
+	}
 });
-
- 
-
-// THIS  MUST  BE  THIS  WAY
-
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
